dca8bcc9b3bd17ab7ec7fb7768990d7e04602653
Shamik69 <k.shanikpal@gmail.com>
2021-01-23 18:44:51 +0530

    Update test-php.php

--- test-php.php
+++ test-php.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php test</title>
+    <link rel="stylesheet" href="css/style0.css">
 </head>
 <body>
     <?php
         $x = 5;
-        $y = 5;
-        echo $x == $y;
-
+        $y = 6;
+        if ($x == $y)
+            echo $x;
+        else
+            echo "$x <br> $y"
+        
     ?>
+
 </body>
 </html>
