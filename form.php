c520074991a61902097d10b25ecd88d15635b8d1
Shamik69 <k.shanikpal@gmail.com>
2021-01-23 22:47:58 +0530

    Update form.php

--- form.php
+++ form.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form data</title>
+    <link rel="stylesheet" href="style0.css">
 </head>
 <body>
     <?php
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $age = $_POST['age'];
-        $m = $_POST['gender'];    
-        $mail = $_POST['mail'];
-        $time = date('D/M/Y H:i:s');
-        print("name: $fname $lname<br>");
-        print("age: $age<br>");
-        print("gender: $m <br>");
-        print("mail: $mail");
-        print("submit time: $time")
+        foreach($_POST as $result) {
+            if($result!=null){
+                $s = array_keys($_POST, $result);
+                if($s!=null){
+                    foreach($s as $a){
+                        echo "$a: $result <br>";
+                    }
+                }
+            }                        
+        }
         
     ?>
 </body>
