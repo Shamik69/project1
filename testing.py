3d4df4bd658fbf25a33827060e67dc270314ff2c
Shamik69 <k.shanikpal@gmail.com>
2021-01-23 21:30:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -14,12 +14,17 @@ driver.find_element_by_id('lname').send_keys('Pal')
 driver.find_element_by_id('age').send_keys('20')
 
 def click(element):
+    time.sleep(0.5)
     webdriver.ActionChains(driver).click(element).perform()
+    time.sleep(0.5)
+
+
+driver.find_element_by_id('mail').send_keys('example@mail.com')
 
 click(driver.find_element_by_id('male'))
 click(driver.find_element_by_id('Submit'))
 
-time.sleep(2)
+time.sleep(5)
 driver.close()
 quit()
 
