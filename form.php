f9d24de3f20ce90091b28be9f36721ba1f1bbe09
Shamik69 <k.shanikpal@gmail.com>
2021-02-04 14:42:12 +0530

    Update form.php

--- form.php
+++ form.php
@@ -11,8 +11,6 @@
         include 'credentials.php';
         include 'db.php';
         $conn = new mysqli($server, $u_name, $pwd);
-        $keys= array_keys($_POST);
-        $results= $_POST;
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -23,10 +21,12 @@
             } else {
             echo "Error creating database: <br>" . $conn->error;
             }
-        }else{
-            $conn = new mysqli($server, $u_name, $pwd, $db_name);
         }
+        $conn = new mysqli($server, $u_name, $pwd, $db_name);
 
+        $qry= "SELECT * FROM $table1";
+        $results= mysqli_fetch_array(mysqli_query($conn, $qry), MYSQLI_ASSOC);
+        
         if(mysqli_query($conn, "SELECT * FROM $table0")===false){
             if (create_table($table0, $conn)){
             data_inject($conn, $table0, $results);
@@ -35,16 +35,8 @@
         } else{
             data_inject($conn, $table0, $results);
         }
-        foreach($results as $result) {
-            if($result!=null){
-                $s = array_keys($results, $result);
-                if($s!=null){
-                    foreach($s as $a){
-                        echo "<b>$a:</b> <i class= cls>$result</i><br>";
-                    }
-                }
-            }                        
-        }
+        
+        print_all($results);
     ?>
     <br><br>
     <a href="index.php" class="cls" id="index">homepage</a>
