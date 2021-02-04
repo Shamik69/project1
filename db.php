368bad038c76d269f332491b375a55bdb91bb589
Shamik69 <k.shanikpal@gmail.com>
2021-02-04 12:10:42 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,62 @@
+<?php
+    function data_inject($connention, $table, $results){
+        // if table already exists
+        $keys= array_keys($results);
+        $cols= "";
+        $values= "";
+        foreach ($keys as $key) {
+            if(count($keys)-array_search($key, $keys)!=1){
+                $cols= $cols."$key,";
+                if($results[$key]==null){
+                    $values=  $values."'Unknown',";
+                }else{
+                    $values=  $values."'$results[$key]',";
+                }
+            }else{
+                $cols= $cols."$key";
+                if($results[$key]==null){
+                    $values=  $values."'Unknown'";
+                }else{
+                    $values=  $values."'$results[$key]'";
+                }
+            }
+        }
+        $insert_qry= "INSERT INTO $table ($cols)\n
+                        VALUES ($values)";
+        
+        if ($connention->query($insert_qry) === TRUE) {
+            echo "New record created successfully<br>";
+          } else {
+            echo "Error: " . $insert_qry . "<br>" . $connention->error. "<br>";
+          }
+    }
+
+    
+    function create_db($db_name, $conn){
+        $create_db = "CREATE DATABASE $db_name";
+            if ($conn->query($create_db) === TRUE) {
+            echo "Database created successfully <br>";
+            return true;
+            } else {
+            echo "Error creating database: <br>" . $conn->error;
+            return false;
+            }
+    }
+
+    function create_table($table_name, $conn){
+        $create_table = "CREATE TABLE $table_name(
+            f_name VARCHAR(20),
+            l_name VARCHAR(20),
+            age INT,
+            gender VARCHAR(10), 
+            edu VARCHAR(50), 
+            edu_status VARCHAR(50),
+            mail VARCHAR(50))";
+        if (mysqli_query($conn, $create_table)) {
+        echo "Table created successfully";
+        return true;
+        } else {
+        echo "Error creating table: <br>" . $conn->error;
+        return false;
+        }
+    }
