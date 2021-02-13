d066cef94d62f38fe61ce4ee6419653893504409
Shamik69 <k.shanikpal@gmail.com>
2021-02-13 12:30:49 +0530

    Update delete_db.php

--- testing/delete_db.php
+++ testing/delete_db.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
             include 'C:\xampp\htdocs\project1\credentials.php';
@@ -14,11 +16,11 @@
                 die("Connection failed: " . $conn->connect_error);
             }
             
-            if(mysqli_query($conn, "SELECT * FROM $table1")===false){
+            if(mysqli_query($conn, "SELECT * FROM keys")===false){
                 echo "<br>";
                 echo "something is fishy<br> $conn->error";
             }else{
-                $drop_table = "DROP TABLE $table1";
+                $drop_table = "DROP TABLE keys";
                 if (mysqli_query($conn, $drop_table)) {
                 echo "Table dropped successfully";
                 } else {
@@ -28,4 +30,5 @@
             }
     ?>
 </body>
+
 </html>
