4ae3e779e73fdf86df88f853c3f9065cf126fc32
Shamik69 <k.shanikpal@gmail.com>
2021-02-14 23:24:49 +0530

    Revert "Update delete_db.php"
    This reverts commit d066cef94d62f38fe61ce4ee6419653893504409.

--- testing/delete_db.php
+++ testing/delete_db.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <?php
             include 'C:\xampp\htdocs\project1\credentials.php';
@@ -16,11 +14,11 @@
                 die("Connection failed: " . $conn->connect_error);
             }
             
-            if(mysqli_query($conn, "SELECT * FROM keys")===false){
+            if(mysqli_query($conn, "SELECT * FROM $table1")===false){
                 echo "<br>";
                 echo "something is fishy<br> $conn->error";
             }else{
-                $drop_table = "DROP TABLE keys";
+                $drop_table = "DROP TABLE $table1";
                 if (mysqli_query($conn, $drop_table)) {
                 echo "Table dropped successfully";
                 } else {
@@ -30,5 +28,4 @@
             }
     ?>
 </body>
-
 </html>
